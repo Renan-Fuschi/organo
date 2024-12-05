a44562ba75a7291b51bbb53a9c3668b723093a66
Henrique Carvalho <henrique.carvalho@sirius.com.br>
2024-12-05 18:51:54 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulário';
+import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
@@ -15,11 +15,11 @@ function App() {
       nome: 'Front-End',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF'
-    },  
+    },
     {
       nome: 'Data Science',
       corPrimaria: '#A6D157',
-      corSecundaria: 'FOF8E2'
+      corSecundaria: '#F0F8E2'
     },
     {
       nome: 'Devops',
@@ -40,26 +40,29 @@ function App() {
       nome: 'Inovação e Gestão',
       corPrimaria: '#FF8A29',
       corSecundaria: '#FFEEDF'
-    },
+    }
   ]
 
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    setColaboradores([colaboradores, colaborador])
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      
+
       {times.map(time => <Time 
-      key={time.nome} 
-      nome={time.nome} 
-      corPrimaria={time.corPrimaria} 
-      corSecundaria={time.corSecundaria}
-      colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}/>)}
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+
     </div>
   );
 }
